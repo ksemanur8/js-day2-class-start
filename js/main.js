8c177805a3ee7484fa8ba9baf405c49450297a58
ksemanur <59751315+ksemanur8@users.noreply.github.com>
2021-09-15 07:36:58 -0400

    Table complete

--- js/main.js
+++ js/main.js
@@ -19,21 +19,21 @@ for (var i=0; i<teams.length; i++){
   //console.log("inside the loop " + i);
   console.log(teams[i]);
 
-  if (losses[i] >= 60){
+  if (losses[i] >= 60){ 
     console.log("This team is horrible!");
   }else{
     console.log('This team is not that bad');
   }
 
 
-  htmlBuilder += teams[i] + " wins: " + wins[i] + " Losses: " + losses[i] + "</br>";
+  htmlBuilder += teams[i] + ": Wins: " + wins[i] + " Losses: " + losses[i] + "</br>";
   console.log(htmlBuilder);
 }
 htmlBuilder += "</div>";
 document.getElementById('teams').innerHTML=htmlBuilder;
 
 
-//logic to determin the team with the most wins.
+//logic to determine the team with the most wins.
 
 var tempHighWins = 0;
 var tempLeader = 0;
@@ -46,3 +46,15 @@ for (var i=0; i<teams.length; i++){
 }
 
 console.log("The team with the most wins of " + tempHighWins + " is " + teams[tempLeader]);
+
+tableBuilder = '<h2>National League</h2> <table><tr> <th>Team</th><th>Wins</th><th>Losses</th> </tr>';
+for (var j=0; j < teams.length; j++) {
+  if (j == tempLeader) {
+    console.log("entered at j=" + j);
+    tableBuilder += `<tr><td><strong>${teams[j]}</strong></td><td><strong>${losses[j]}</strong></td><td><strong>${teams[j]}</strong></td> </tr>`;
+  } else {
+    tableBuilder += `<tr><td>${teams[j]}</td><td>${losses[j]}</td><td>${teams[j]}</td></tr>`;
+  }
+}
+tableBuilder += '</table>';
+document.getElementById("teams").innerHTML += tableBuilder;
